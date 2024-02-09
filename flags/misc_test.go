1d3c72452d0873da9708c88904705bd0697535f4
Jesse Schmidt <jschmidt@aerospike.com>
2024-02-09 12:07:50 -0500

    feat: TOOLS-2836 add build to version

--- flags/misc_test.go
+++ flags/misc_test.go
@@ -11,7 +11,7 @@ func TestSetupRoot(t *testing.T) {
 	rootCmd := &cobra.Command{}
 	appLongName := "Some Random Tool"
 
-	SetupRoot(rootCmd, appLongName)
+	SetupRoot(rootCmd, appLongName, "1.2.3")
 
 	// Verify that the "help" flag is registered
 	helpFlag, err := rootCmd.PersistentFlags().GetBool("help")
@@ -19,11 +19,22 @@ func TestSetupRoot(t *testing.T) {
 	assert.False(t, helpFlag)
 
 	// Verify the version template is set correctly
-	expectedVersionTemplate := "Some Random Tool\n{{printf \"Version %s\" .Version}}\n"
+	expectedVersionTemplate := "Some Random Tool\nVersion 1.2.3\n"
 	assert.Equal(t, expectedVersionTemplate, rootCmd.VersionTemplate())
 
 	// Verify that the "version" flag is registered
 	versionFlag, err := rootCmd.PersistentFlags().GetBool("version")
 	assert.NoError(t, err)
 	assert.False(t, versionFlag)
+}
+
+func TestSetupRootWithBuild(t *testing.T) {
+	rootCmd := &cobra.Command{}
+	appLongName := "Some Random Tool"
+
+	SetupRoot(rootCmd, appLongName, "1.2.3-9-g12345")
+
+	// Verify the version template is set correctly
+	expectedVersionTemplate := "Some Random Tool\nVersion 1.2.3\nBuild g12345\n"
+	assert.Equal(t, expectedVersionTemplate, rootCmd.VersionTemplate())
 }
