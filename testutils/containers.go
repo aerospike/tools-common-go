766ab8f3da4b77011d1509e2d29ae62e7307c895
Jesse S <jschmidt@aerospike.com>
2024-03-08 14:55:27 -0800

    Fix testutils featkey (#15)
    * fix: Fix feature key functionality when using EE container

--- testutils/containers.go
+++ testutils/containers.go
@@ -170,9 +170,10 @@ func GetAerospikeContainerName(index int) string {
 	return ContainerPrefix + fmt.Sprintf("%d", index)
 }
 
-func createConfigFile(portBase int, accessAddress, peerConnection, featKey string) (string, error) {
-	if featKey != "" {
-		featKey = fmt.Sprintf("feature-key-file %s", featKey)
+func createConfigFile(portBase int, accessAddress, peerConnection, featKeyEnvVar string) (string, error) {
+	featKeyLine := ""
+	if featKeyEnvVar != "" {
+		featKeyLine = fmt.Sprintf("feature-key-file env-b64:%s", featKeyEnvVar)
 	}
 
 	templateInput := struct {
@@ -185,7 +186,7 @@ func createConfigFile(portBase int, accessAddress, peerConnection, featKey strin
 		FabricPort     int
 		InfoPort       int
 	}{
-		FeatureKeyFile: featKey,
+		FeatureKeyFile: featKeyLine,
 		AccessAddress:  accessAddress,
 		PeerConnection: peerConnection,
 		Namespace:      "test",
@@ -267,13 +268,17 @@ func RunAerospikeContainer(
 
 	log.Printf("Starting container %s", name)
 
+	featKeyEnvVar := "FEATURES"
+
 	// Uncomment if multi-node EE tests are needed
 	featKey := os.Getenv("FEATKEY")
 	if featKey == "" {
+		featKeyEnvVar = ""
+
 		log.Printf("FEATKEY is not set. Multi-node EE tests will not be run")
 	}
 
-	confFile, err := createConfigFile(portBase, ip, peerConnection, featKey)
+	confFile, err := createConfigFile(portBase, ip, peerConnection, featKeyEnvVar)
 
 	if err != nil {
 		log.Printf("Unable to create config file for container %s: %s", name, err)
@@ -299,10 +304,10 @@ func RunAerospikeContainer(
 	}
 
 	config := container.Config{
-		Image:    Image,
-		Hostname: ip,
-		Cmd:      cmd,
-		Tty:      true,
+		Image: Image,
+		// Hostname: ip, # Use host network
+		Cmd: cmd,
+		Tty: true,
 		ExposedPorts: nat.PortSet{
 			nat.Port(ports[0]): {},
 			nat.Port(ports[1]): {},
@@ -312,7 +317,7 @@ func RunAerospikeContainer(
 	}
 
 	if featKey != "" {
-		config.Env = append(config.Env, fmt.Sprintf("FEATURES=%s", featKey))
+		config.Env = append(config.Env, fmt.Sprintf("%s=%s", featKeyEnvVar, featKey))
 	}
 
 	if err != nil {
